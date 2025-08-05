8493fbf286f34d79071a3eaff677c3fe5bf0b0ed
DreamerAkhilesh <imakhileshpratap@gmail.com>
2025-08-06 02:27:14 +0530

    Fix import error - getCurrentUser should be imported from authThunks

--- client/src/components/auth/ProtectedRoute.js
+++ client/src/components/auth/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Navigate, useLocation, Outlet } from 'react-router-dom';
-import { getCurrentUser } from '../../store/slices/authSlice';
+import { getCurrentUser } from '../../store/thunks/authThunks';
 import PropTypes from 'prop-types';
 
 const ProtectedRoute = ({ roles = [] }) => {
